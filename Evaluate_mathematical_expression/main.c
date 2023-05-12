37e90ed2771bcadfed9a7019a310f602e2c0ad4f
TeuerStein <oleksandr.sokolov10010@gmail.com>
2023-05-12 20:56:41 +0200

    Adjusted 'Evaluate_mathematical_expression' algorithm, so it could remove spaces between unary minus and numbers before parsing it

--- Evaluate_mathematical_expression/main.c
+++ Evaluate_mathematical_expression/main.c
@@ -5,36 +5,48 @@
 
 #define MAX_SIZE 100
 
+typedef struct Node {
+    double data;
+    struct Node* next;
+} Node;
+
 typedef struct Stack {
-    double arr[MAX_SIZE];
-    int top;
+    Node* top;
 } Stack;
 
 void push(Stack* stack, double num) {
-    if(stack->top < MAX_SIZE - 1) {
-        stack->arr[(stack->top)++] = num;
-    } else {
-        printf("Stack overflow. Can't push %lf onto the stack.\n", num);
-        exit(1);
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Error: could not allocate memory for new node\n");
+        return;
     }
+
+    newNode->data = num;
+    newNode->next = stack->top;
+    stack->top = newNode;
 }
 
 double pop(Stack* stack) {
-    if(stack->top != -1) {
-        return stack->arr[(stack->top)--];
-    } else {
-        printf("Stack underflow. Can't pop from the stack.\n");
-        exit(1);
+    if (stack->top == NULL) {
+        printf("Error: stack underflow. Can't pop from the stack.\n");
+        return -1; // or some other value to indicate an error
     }
+
+    Node* topNode = stack->top;
+    double data = topNode->data;
+    stack->top = topNode->next;
+
+    free(topNode);
+    return data;
 }
 
 double peek(Stack* stack) {
-    if(stack->top != -1) {
-        return stack->arr[stack->top];
-    } else {
-        printf("Stack underflow. Can't peek the stack.\n");
-        exit(1);
+    if (stack->top == NULL) {
+        printf("Error: stack is empty.\n");
+        return -1;
     }
+
+    return stack->top->data;
 }
 
 int precedence(char operator) {
@@ -82,81 +94,123 @@ void performOperation(Stack* stack, Stack* opStack) {
     }
 }
 
+char* preprocess(const char* expression) {
+    char* newExpression = malloc(strlen(expression) + 1);
+    int j = 0;
+    for (int i = 0; expression[i] != '\0'; i++) {
+        if (expression[i] == '-' && expression[i + 1] == ' ') {
+            newExpression[j++] = expression[i++];
+            while (expression[i] == ' ') i++;
+        }
+        newExpression[j++] = expression[i];
+    }
+    newExpression[j] = '\0';
+    return newExpression;
+}
+
+double parseNumber(const char** expression) {
+    double result = 0.0;
+    int decimalPlace = 0;
+
+    while (isdigit(**expression) || **expression == '.') {
+        if (**expression == '.') {
+            if (decimalPlace) {  // if decimalPlace is not 0, we have already encountered a decimal point
+                printf("Error: Invalid number with multiple decimal points\n");
+                exit(EXIT_FAILURE);
+            }
+            decimalPlace = 1;
+        } else {
+            if (decimalPlace) {
+                decimalPlace *= 10;
+                result += ((double)**expression - '0') / decimalPlace;
+            } else {
+                result = result * 10.0 + (**expression - '0');
+            }
+        }
+        (*expression)++;
+    }
+
+    return result;
+}
+
 double calculate(const char* expression) {
-    Stack stack;
-    stack.top = -1;
-    Stack opStack;
-    opStack.top = -1;
-    char currentToken[MAX_SIZE];
-    int tokenIndex = 0;
-    int exprLength = strlen(expression);
+    Stack operandStack;
+    operandStack.top = NULL;
+    Stack operatorStack;
+    operatorStack.top = NULL;
 
-    for (int i = 0; i <= exprLength; i++) {
-        char currentChar = expression[i];
+    int isUnaryMinus = 0;
 
-        // Check if currentChar is a digit, a decimal point, or a negative sign followed by a digit or parenthesis
-        if (isdigit(currentChar) || currentChar == '.' ||
-            (currentChar == '-' && (isdigit(expression[i + 1]) || expression[i + 1] == '.' || expression[i + 1] == '('))) {
-            currentToken[tokenIndex++] = currentChar;
+    while (*expression) {
+        if (isspace(*expression)) {
+            // Ignore white spaces
+            expression++;
             continue;
         }
-
-        if (tokenIndex > 0) {
-            currentToken[tokenIndex] = '\0';
-            push(&stack, atof(currentToken));
-            tokenIndex = 0;
-        }
-
-        if (strchr("+-*/", currentChar) != NULL) {
-            while (opStack.top != -1 && precedence(peek(&opStack)) >= precedence(currentChar)) {
-                if(stack.top >= 1) {
-                    performOperation(&stack, &opStack);
-                } else {
-                    break;
-                }
+        if (*expression == '(') {
+            isUnaryMinus = 1;
+            push(&operatorStack, '(');
+        } else if (*expression == ')') {
+            while (operatorStack.top > 0 && top(&operatorStack) != '(') {
+                processOperation(&operandStack, &operatorStack);
             }
-            push(&opStack, currentChar);
-        } else if (currentChar == '(') {
-            push(&opStack, currentChar);
-        } else if (currentChar == ')') {
-            while (opStack.top != -1 && peek(&opStack) != '(') {
-                if(stack.top >= 1) {
-                    performOperation(&stack, &opStack);
-                } else {
-                    break;
-                }
+            if (operatorStack.top == 0 || top(&operatorStack) != '(') {
+                printf("Error: Mismatched parentheses.\n");
+                freeStack(&operandStack);
+                freeStack(&operatorStack);
+                exit(EXIT_FAILURE);
             }
-            if(opStack.top != -1) {
-                pop(&opStack); // Pop the '('
+            pop(&operatorStack); // Pop the '(' from the stack
+            isUnaryMinus = 0;
+        } else if (isdigit(*expression)) {
+            double number = parseNumber(&expression);
+            if (isUnaryMinus) {
+                push(&operandStack, -number);
+                isUnaryMinus = 0;
             } else {
-                printf("Mismatched parentheses in expression.\n");
-                exit(1);
+                push(&operandStack, number);
             }
-        } else if (currentChar != '\0' && currentChar != ' ') {
-            printf("Error: Invalid character %c in expression.\n", currentChar);
-            exit(1);
-        }
-    }
-
-    while (opStack.top != -1) {
-        if(stack.top >= 1) {
-            performOperation(&stack, &opStack);
+            expression--;
+        } else if (isOperator(*expression)) {
+            while (operatorStack.top > 0 && top(&operatorStack) != '(' &&
+                   hasHigherPrecedence(top(&operatorStack), *expression)) {
+                processOperation(&operandStack, &operatorStack);
+            }
+            if (*expression == '-' && (isUnaryMinus || operatorStack.top == 0 ||
+                                       top(&operatorStack) == '(')) {
+                isUnaryMinus = 1;
+            } else {
+                push(&operatorStack, *expression);
+                isUnaryMinus = 0;
+            }
         } else {
-            break;
+            printf("Error: Invalid character %c\n", *expression);
+            freeStack(&operandStack);
+            freeStack(&operatorStack);
+            exit(EXIT_FAILURE);
         }
+        expression++;
     }
 
-    if(stack.top > 0) {
-        printf("Invalid expression. Remaining operands in stack.\n");
-        exit(1);
+    while (operatorStack.top > 0) {
+        if (top(&operatorStack) == '(') {
+            printf("Error: Mismatched parentheses.\n");
+            freeStack(&operandStack);
+            freeStack(&operatorStack);
+            exit(EXIT_FAILURE);
+        }
+        processOperation(&operandStack, &operatorStack);
     }
 
-    if(stack.top < 0) {
-        printf("Not enough operands for operation.\n");
-        exit(1);
+    if (operandStack.top != 1) {
+        printf("Error: Invalid expression.\n");
+        freeStack(&operandStack);
+        freeStack(&operatorStack);
+        exit(EXIT_FAILURE);
     }
 
-    return pop(&stack);
+    double result = pop(&operandStack);
+    return result;
 }
 
 int main() {
